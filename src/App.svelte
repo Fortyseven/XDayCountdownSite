dff5c4ee38cd7c4a91618e7a48face34fb5a3c56
Fortyseven <fortyseven@network47.org>
2022-01-23 18:09:46 -0500

    Let's move these together

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
     import { onMount } from "svelte";
-    // import timemachine from "timemachine";
     import Dobbsface from "./components/Dobbsface.svelte";
     import Fullscreen from "./components/Fullscreen.svelte";
 
+    // import timemachine from "timemachine";
     // timemachine.config({
-    //     dateString: "2022-07-05 6:59:55",
+    //     dateString: "2022-07-05 6:59:57",
     //     tick: true,
     // });
 </script>
